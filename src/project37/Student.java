b9c6c827873f465da19246cbe862d44332364c00
Sara Usevska <sarausevska42@yahoo.com>
2023-12-15 10:00:33 +0100

    first commit

---
+++ src/project37/Student.java
@@ -0,0 +1,22 @@
+package project37;
+
+class Student {
+int index;
+String ime;
+static String fakultet ="ITS";//staticna promenliva
+
+Student(int r,String n){
+	index = r;
+	ime= n; 
+ }
+	void display() {
+		System.out.println(index + " " + ime +" " + fakultet);
+	}
+	public static void main(String args[]) {
+		Student s1= new Student(111,"Petar");
+		Student s2= new Student(222,"Marko");
+   s1.display();
+   s2.display();
+	}
+
+}
